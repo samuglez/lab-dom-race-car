6b05adef0e6fcc5f2a1d4f62fcf3a55ae0aedd73
unknown <samuglez90@gmail.com>
2025-01-24 13:01:33 +0100

    Hecho a falta de los bonus

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
+  // const gameContainer = document.getElementById("game-container");
+
+  
 
   startButton.addEventListener("click", function () {
     startGame();
@@ -10,3 +13,63 @@ window.onload = function () {
     console.log("start game");
   }
 };
+window.onload = function () {
+  const startButton = document.getElementById("start-button");
+  const restartButton = document.getElementById("restart-button");
+  let game; // added
+
+  startButton.addEventListener("click", function () {
+    startGame();
+  });
+
+  function startGame() {
+    console.log("start game");
+    game = new Game(); // added
+
+    game.start(); // added
+  }
+  function handleKeydown(event) {
+    const key = event.key;
+    const possibleKeystrokes = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown",
+    ];
+
+    // Check if the pressed key is in the possibleKeystrokes array
+    if (possibleKeystrokes.includes(key)) {
+      event.preventDefault();
+
+      // Update player's directionX and directionY based on the key pressed
+      switch (key) {
+        case "ArrowLeft":
+          game.player.directionX = -1;
+          break;
+        case "ArrowUp":
+          game.player.directionY = -1;
+          break;
+        case "ArrowRight":
+          game.player.directionX = 1;
+          break;
+        case "ArrowDown":
+          game.player.directionY = 1;
+          break;
+      }
+    }
+  }
+
+  // Add the handleKeydown function as an event listener for the keydown event
+  window.addEventListener("keydown", handleKeydown);
+  // Add an event listener to the restart button
+  restartButton.addEventListener("click", function () {
+    // Call the restartGame function when the button is clicked
+    restartGame();
+  });
+
+  // The function that reloads the page to start a new game
+  function restartGame() {
+    location.reload();
+  }
+};
+
